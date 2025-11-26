d562fd7bd4018c512d8eaaa8d5275abe63d676e7
BitNebulaV7 <shivaq1994@gmail.com>
2025-11-26 10:26:39 +0330

    Add transpose step: rearrange image axes to separate RGB channels

--- image_generator.py
+++ image_generator.py
@@ -11,7 +11,17 @@ image[:, :, 1] = np.random.randint(0, 256, (50, 50))  # Green
 image[:, :, 2] = np.random.randint(0, 256, (50, 50))  # Blue
 
 image[0:10, 0:10] = [0, 0, 255]
+vector = image.reshape(-1)
+print(vector)
+print("Vector shape:", vector.shape) 
 
+image_back = image.reshape(50,50,3)
+print(image_back)
+print("Image shape:", image_back.shape) 
+
+transposed = np.transpose(image,(2,0,1))
+print("Original shape:", image.shape)     
+print("Transposed shape:", transposed.shape) 
 plt.imshow(image)
 plt.axis("off")
 plt.show()
